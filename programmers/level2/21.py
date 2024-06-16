acb2a7e086acd9288b5e257efd130daacc9fc14b
Seongsu Kim <100525295+sseongsukim@users.noreply.github.com>
2024-06-16 12:15:58 +0900

    Review

--- programmers/level2/21.py
+++ programmers/level2/21.py
@@ -1,6 +1,5 @@
 """
-
-
+n^2 배열 자르기
 """
 def solution(n, left, right):
     answer = []
@@ -12,4 +11,4 @@ def solution(n, left, right):
     return answer
 
 print(solution(3, 2, 5)) # [3, 2, 2, 3]
-print(solution(4, 7, 14)) # [4,3,3,3,4,4,4,4]
+print(solution(4, 7, 14)) # [4, 3, 3, 3, 4, 4, 4, 4]
